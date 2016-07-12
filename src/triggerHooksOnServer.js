51fdc35cc3c981d96c2d9b360097bd4c74b09123
kou_hin <kou_hin@cyberagent.co.jp>
2016-07-12 11:21:20 +0900

    use redux on server side

--- src/triggerHooksOnServer.js
+++ src/triggerHooksOnServer.js
@@ -1,53 +1,11 @@
-import getAllComponents from './getAllComponents';
-import { routerHookPropName } from './routerHooks';
+import configureStore from './configureStore';
+import { reloadAllComponents } from './routerModule';
 
 export default function triggerHooksOnServer(renderProps, hooks = [], locals) {
-  const componentsProps = new Map();
-  const components = getAllComponents(renderProps.components);
-
-  function setComponentProps(Component) {
-    return props => {
-      componentsProps.set(Component, {
-        ...(componentsProps.get(Component) || {}),
-        ...props,
-      });
-    };
-  }
-
-  function getComponentProps(Component) {
-    return () => {
-      const props = componentsProps.get(Component);
-      if (!props) {
-        setComponentProps(Component)({ loading: false });
-        return componentsProps.get(Component);
-      }
-      return props;
-    };
-  }
-
-  function getLocals(Component) {
-    const {
-      location,
-      params,
-    } = renderProps;
-    const resultLocals = (typeof locals === 'function')
-      ? locals(Component) : locals;
-    return {
-      ...resultLocals,
-      location,
-      params,
-      setProps: setComponentProps(Component),
-      getProps: getComponentProps(Component),
-    };
-  }
-  const promises = components.map(Component => {
-    const routerHooks = Component[routerHookPropName];
-    const finallocals = getLocals(Component);
-    return hooks
-      .map(k => routerHooks[k])
-      .filter(f => f)
-      .reduce((total, current) => total.then(() => current(finallocals))
-      , Promise.resolve());
+  const store = configureStore({
+    ...locals,
+    routerWillEnterHooks: hooks,
   });
-  return Promise.all(promises).then(() => componentsProps);
+  return store.dispatch(reloadAllComponents(renderProps.components, renderProps))
+    .then(() => store);
 }
